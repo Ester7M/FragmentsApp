58c986a09d4285e906e6627b989fc5c143918b76
unknown <mulenga.e7@gmail.com>
2021-12-05 20:15:53 +0300

    first commit

---
+++ app/src/main/java/com/fragments/StartFragment.kt
@@ -0,0 +1,26 @@
+package com.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class StartFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_start, container, false)
+
+
+
+
+        return view
+    }
+
+
+}
